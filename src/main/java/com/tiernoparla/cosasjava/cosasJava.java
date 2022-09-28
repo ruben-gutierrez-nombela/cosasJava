3a64131fbaa9be5ada919a4643b60bd62e643b7c
ruben-gutierrez-nombela <ruben.gutierreznombela@tiernoparla.es>
2022-09-28 18:25:48 +0200

    guardado bien el .java

--- src/main/java/com/tiernoparla/cosasjava/cosasJava.java
+++ src/main/java/com/tiernoparla/cosasjava/cosasJava.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.tiernoparla.cosasjava;
+package com.tiernoparla.cosasJava;
 
-/**
- *
- * @author vespertino
- */
 public class cosasJava {
-    
-}
+
+    public static void main(String[] args) {
+        /*
+        // printear nums pares del 1 al 10
+        for (int i = 0; i < 12; i = i + 2) {
+            System.out.println(i);
+        } //for
+        */
+        
+        /*
+        int a = 33+1;
+        int b = 2;
+        System.out.println(a%b);
+        */
+        
+        /*
+        if (false){
+            System.out.println("era true");
+        } //if
+        */
+        
+        /*
+        for (int i = 1; i < 11; i++){
+            if(i%2 == 0){
+                System.out.println(i);
+            } //if
+        } //for
+        */
+        
+        int n = 1;
+        while(n < 11){
+            if(n%2 == 0){
+                System.out.println(n);
+            } //if
+            n++;
+        } //while
+        
+        
+        
+    } //main
+} //cosasJava
